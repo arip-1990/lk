b03acfa136d3f83bd83defadd7c75a0683994a9d
Arip <dev@arip.info>
2022-06-17 13:23:26 +0300

    fix import timecard

--- api/app/Console/Commands/TimeCardCommand.php
+++ api/app/Console/Commands/TimeCardCommand.php
@@ -19,13 +19,28 @@ class TimeCardCommand extends Command
     public function handle(): int
     {
         $this->info('Import timeCard');
-        $path = $this->copyFile();
+        $path = Storage::disk('local')->path('tmp/Табель.xlsx');
+
+        try {
+            $this->copyFile($path);
+        }
+        catch (\DomainException $e) {
+            if (TimeCard::query()->whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])->count()) {
+                $this->warn($e->getMessage());
+                return 1;
+            }
+        }
+        catch (\RuntimeException $e) {
+            $this->warn($e->getMessage());
+            return 1;
+        }
 
         $reader = IOFactory::createReader('Xlsx');
         $reader->setReadDataOnly(true);
         $sheet = $reader->load($path);
 
         $store = null;
+        $updated = [];
         foreach ($sheet->getActiveSheet()->toArray() as $row) {
             try {
                 if (is_numeric($row[0]) and $store) {
@@ -41,21 +56,24 @@ class TimeCardCommand extends Command
                     }
 
                     $timeCard = TimeCard::query()->where('user', trim($row[1]))
-                        ->where('store_id', $store->id)->where('post', trim($row[2]))->first();
+                        ->where('store_id', $store->id)->where('post', trim($row[2]))
+                        ->where('created_at', '>', Carbon::now()->startOfMonth())->first();
                     if ($timeCard) {
                         $timeCard->update([
                             'time_card_number' => trim($row[3]) ?? '',
                             'normative_id' => $normative->id
                         ]);
+                        $updated[] = $timeCard->id;
                     }
                     else {
-                        TimeCard::query()->create([
+                        $timeCard = TimeCard::query()->create([
                             'user' => trim($row[1]),
                             'post' => trim($row[2]),
-                            'store_id' => $store,
+                            'store_id' => $store->id,
                             'time_card_number' => trim($row[3]) ?? '',
                             'normative_id' => $normative->id
                         ]);
+                        $updated[] = $timeCard->id;
                     }
                 }
                 elseif (is_string($row[0]) and strlen($row[0]) > 5) {
@@ -65,35 +83,34 @@ class TimeCardCommand extends Command
             catch (\DomainException $e) {}
         }
 
+        TimeCard::query()->whereNotIn('id', $updated)
+            ->where('created_at', '>', Carbon::now()->startOfMonth())->delete();
+
         $this->info('Done!');
 
         return 0;
     }
 
-    private function copyFile(): string
+    private function copyFile(string $path): void
     {
-        $path = Storage::disk('local')->path('tmp/Табель.xlsx');
         $connection = ssh2_connect('192.168.2.16');
         if (!ssh2_auth_password($connection, 'arip', 'Arip_1990'))
-            throw new \DomainException('Ошибка подключеня!');
+            throw new \RuntimeException('Ошибка подключеня!');
 
         $sftp = ssh2_sftp($connection);
         $statInfo = ssh2_sftp_stat($sftp, '/E:/soc_apteka/Табель ДФ/Табель.xlsx');
         if ($statInfo['size'] <= 0)
-            throw new \DomainException('Пустой файл!');
+            throw new \RuntimeException('Пустой файл!');
 
-        $timeCard = TimeCard::query()->orderByDesc('created_at')->first();
-        if ($timeCard and $timeCard->created_at >= Carbon::parse($statInfo['mtime']))
+        if (Carbon::parse(Storage::disk('local')->lastModified('tmp/Табель.xlsx')) >= Carbon::parse($statInfo['mtime']))
             throw new \DomainException('Нет изменений в файле!');
 
         $stream = @fopen('ssh2.sftp://' . intval($sftp) . '/E:/soc_apteka/Табель ДФ/Табель.xlsx', 'r');
         if (!$stream)
-            throw new \DomainException("Не получилось открыть файл: /E:/soc_apteka/Табель ДФ/Табель.xlsx");
+            throw new \RuntimeException("Не получилось открыть файл: /E:/soc_apteka/Табель ДФ/Табель.xlsx");
 
         $contents = stream_get_contents($stream);
         file_put_contents ($path, $contents);
         @fclose($stream);
-
-        return $path;
     }
 }
